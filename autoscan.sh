b32202127cee14dbc8f31c63c112b08b5a1718e6
Checkn8 <69409917+NathanLundner@users.noreply.github.com>
2020-10-16 11:40:55 -0400

    Update autoscan.sh

--- autoscan.sh
+++ autoscan.sh
@@ -3,24 +3,35 @@
 # Network enumeration script written by checkn8
 # Just a quick script to help when using Nmap
 
+yellow="\e[93m"
+
+reset="\e[0m"
+
 printf "Your IP is: $2 \n";
 
-printf "[+] Pinging Network...\n";
-nmap -sn -T4 $1 >live_hosts.txt;
+# Create a dir to output scans with date and time
+currentDate=`date +"%d-%b-%Y_%H:%M"` ;
+currentDate=$currentDate | cut -c 1 ;
+printf "[+] Saving Scans in /scans_$currentDate \n";
+mkdir scans_$currentDate ;
 
-printf '\n[+] Live Targets: \n';
-cat live_hosts.txt | grep "Nmap scan" | cut -d " " -f 5 | grep -v "$2";
-cat live_hosts.txt | grep "Nmap scan" | cut -d " " -f 5 | grep -v "$2" > live_ip.txt;
-printf '\n';
+printf "[+] Pinging Network...\n";
+nmap -sn -T4 $1 >scans_$currentDate/live_mac_ip.txt;
+
+echo -e "\n[+] Live Targets: ${yellow}\n";
+cat scans_$currentDate/live_mac_ip.txt | grep "Nmap scan" | cut -d " " -f 5 | grep -v "$2";
+cat scans_$currentDate/live_mac_ip.txt | grep "Nmap scan" | cut -d " " -f 5 | grep -v "$2" > scans_$currentDate/live_ips.txt;
+printf "${reset}\n";
 
 printf '[+] Running port and version enumeration: (nmap -T4 -p- -A) \n';
 
-for i in $(<live_ip.txt);do
-# nmap -sC -sV -oA i ;
-	printf "\n[+] Scanning: $i";
+# Loops a nmap scan for every target.
+for i in $(<scans_$currentDate/live_ips.txt);do
+        #-oA scans_$currentDate/$i for multiple output files. 
+	echo -e "${yellow}\n[+] Scanning: $i${reset}";
 	printf "\n";
-	nmap -T4 -p- -A -vv -oN $i.log $i ;
+	nmap -T4 -p- -A -vv -oN scans_$currentDate/$i.log $i ;
 	printf "\n";
 done
 
-printf '[+] Saved Scans';
+printf '[+] Saved Scans\n';
